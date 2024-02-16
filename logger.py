11de706e6b41c645bcea4f1e7cdba4591cf6ef63
hammble <107260407+hammble@users.noreply.github.com>
2024-02-16 13:39:22 +0600

    Update

---
+++ logger.py
@@ -0,0 +1,19 @@
+from loguru import logger
+import colored
+from colored import stylize
+
+user = User("ТОКЕН")
+logger.disable('vkbottle') #Выключает мусор с консоли
+
+@user.on.message(text='<q>')
+async def logger(message: Message, q: str):
+  user_id = message.from_id
+  a = await message.get_user(user_ids=user_id)
+  name = f'{a.first_name} {a.last_name}'
+  current_time = datetime.datetime.now().strftime('%H:%M:%S')
+  current_time = stylize(current_time, colored.fg("cyan"))
+  name = stylize(name, colored.fg("red"))
+  q = stylize(q, colored.fg("cyan"))
+  print(f'[{current_time}] | [{name}] | [{q}]')
+
+user.run_forever
